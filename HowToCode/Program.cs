f0733b204e137335bea55afc6e4dcfb80f51b2c6
Saurabh Srivastava <sauri.jss169@gmail.com>
2018-01-27 00:18:41 +0530

    Push changes

--- HowToCode/Program.cs
+++ HowToCode/Program.cs
@@ -20,22 +20,41 @@ namespace HowToCode
             GetFactorial(input, true);
         }        
 
+        /// <summary>
+        /// Get the factorial of a given number using BigInteger
+        /// </summary>
+        /// <param name="input">Input integer provided by user</param>
 		static void GetFactorial(int input)
 		{
-			BigInteger output = 1;
-			for (int i = 1; i <= input; i++)
-			{
-				output = output * i;
-			}
+            BigInteger output = 1;
+            if (input>=0)
+            {
+                for (int i = 1; i <= input; i++)
+                {
+                    output = output * i;
+                }
 
-			Console.WriteLine(output);
-			Console.Read();
+                Console.WriteLine(output);
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine("Factorial of negative number doesn't exist.");
+                Console.Read();
+            }
 		}
 
+        /// <summary>
+        /// Get the factorial of a given number using Array
+        /// </summary>
+        /// <param name="input">Input integer provided by user</param>
+        /// <param name="usingArray">Boolean variable to check to use array or not.</param>
 		static void GetFactorial(int input, bool usingArray)
 		{
+            //Create array to store output digits
 			int[] res = new int[10000];
 
+            //Initialize the first value of array with 1 and size of the array
 			res[0] = 1;
 			int res_size = 1;
 
@@ -51,18 +70,29 @@ namespace HowToCode
 			Console.Read();
 		}
 
+
+        /// <summary>
+        /// Mulitply the numbers and hold the digits on each position of array
+        /// </summary>
+        /// <param name="i">The number which need to be multiplied</param>
+        /// <param name="res">Array which is used to hold the value of factorial</param>
+        /// <param name="res_size">Size of the array</param>
+        /// <returns></returns>
 		private static int Multiply(int i, int[] res, int res_size)
 		{
+            //Carry in case 
 			int carry = 0;
 
 			for (int j = 0; j < res_size; j++)
 			{
+                //Calculate the product by multiplying the numbers and adding the carry
 				int prod = res[j] * i + carry;
 				res[j] = prod % 10;
 
 				carry = prod / 10;
 			}
 
+            //Increment the array size and add the value of carry in the same
 			while (carry!=0)
 			{
 				res[res_size] = carry % 10;
