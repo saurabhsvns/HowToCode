b55c323736ddac3fbd6e8a81f6366c7b1bc56e8e
Saurabh Srivastava <sauri.jss169@gmail.com>
2018-01-27 22:47:36 +0530

    Added Bubble sort method.

--- HowToCode/Program.cs
+++ HowToCode/Program.cs
@@ -8,26 +8,141 @@ using System.Net.Mail;
 
 namespace HowToCode
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-            Console.WriteLine("Give the number for which factorial is required");
-            int input = Convert.ToInt32(Console.ReadLine());
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BubbleSort(500);
+            //Console.WriteLine("Give the number for which factorial is required");
+            //int input = Convert.ToInt32(Console.ReadLine());
             //Using BigInteger data type
-            GetFactorial(input);
+            //GetFactorial(input);
             //Using Array
-            GetFactorial(input, true);
-        }        
+            //GetFactorial(input, true);
+        }
+
+        static void BubbleSort(int totalItems)
+        {
+            int[] inputArray = new int[totalItems];
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                inputArray[i] = new Random(i + 100).Next();
+            }
+
+            bool swap = false;
+
+            Console.WriteLine("[{0}]", string.Join(", ", inputArray));
+            do
+            {
+                swap = false;
+                for (int i = 0; i < inputArray.Length - 1; i++)
+                {
+                    if (inputArray[i] > inputArray[i + 1])
+                    {
+                        int temp = inputArray[i];
+                        inputArray[i] = inputArray[i + 1];
+                        inputArray[i + 1] = temp;
+                        swap = true;
+                    }
+                }
+            }
+            while (swap);
+
+            Console.WriteLine("[{0}]", string.Join(", ", inputArray));
+
+            //Optimized way
+            int loopLength = inputArray.Length - 1;
+            do
+            {
+                swap = false;
+
+                for (int i = 0; i < loopLength; i++)
+                {
+                    if (inputArray[i] > inputArray[i + 1])
+                    {
+                        int temp = inputArray[i];
+                        inputArray[i] = inputArray[i + 1];
+                        inputArray[i + 1] = temp;
+                        swap = true;
+                    }
+                }
+                loopLength = loopLength - 1;
+            }
+            while (swap);
+
+            Console.WriteLine("[{0}]", string.Join(", ", inputArray));
+
+
+            //Further Optimized way
+            loopLength = inputArray.Length - 1;
+            int newn = 0;
+            do
+            {
+                newn = 0;
+                for (int i = 0; i < loopLength; i++)
+                {
+                    if (inputArray[i] > inputArray[i + 1])
+                    {
+                        int temp = inputArray[i];
+                        inputArray[i] = inputArray[i + 1];
+                        inputArray[i + 1] = temp;
+                        newn = i;
+                    }
+                }
+                loopLength = newn;
+            }
+            while (newn != 0)   ;
+
+            Console.WriteLine("[{0}]", string.Join(", ", inputArray));
+
+            //Using Cocktail Shaker method with running the loop from both ends
+            swap = false;
+
+            Console.WriteLine("[{0}]", string.Join(", ", inputArray));
+            do
+            {
+                swap = false;
+                for (int i = 0; i < inputArray.Length - 1; i++)
+                {
+                    if (inputArray[i] > inputArray[i + 1])
+                    {
+                        int temp = inputArray[i];
+                        inputArray[i] = inputArray[i + 1];
+                        inputArray[i + 1] = temp;
+                        swap = true;
+                    }
+                }
+                if(!swap)
+                {
+                    break;
+                }
+                for (int i = inputArray.Length-1; i <= 0; i++)
+                {
+                    if (inputArray[i] > inputArray[i - 1])
+                    {
+                        int temp = inputArray[i];
+                        inputArray[i] = inputArray[i + 1];
+                        inputArray[i + 1] = temp;
+                        swap = true;
+                    }
+                }
+            }
+            while (swap);
+
+            Console.WriteLine("[{0}]", string.Join(", ", inputArray));
+
+        }
+
+
 
         /// <summary>
         /// Get the factorial of a given number using BigInteger
         /// </summary>
         /// <param name="input">Input integer provided by user</param>
 		static void GetFactorial(int input)
-		{
+        {
             BigInteger output = 1;
-            if (input>=0)
+            if (input >= 0)
             {
                 for (int i = 1; i <= input; i++)
                 {
@@ -42,7 +157,7 @@ namespace HowToCode
                 Console.WriteLine("Factorial of negative number doesn't exist.");
                 Console.Read();
             }
-		}
+        }
 
         /// <summary>
         /// Get the factorial of a given number using Array
@@ -50,25 +165,25 @@ namespace HowToCode
         /// <param name="input">Input integer provided by user</param>
         /// <param name="usingArray">Boolean variable to check to use array or not.</param>
 		static void GetFactorial(int input, bool usingArray)
-		{
+        {
             //Create array to store output digits
-			int[] res = new int[10000];
+            int[] res = new int[10000];
 
             //Initialize the first value of array with 1 and size of the array
-			res[0] = 1;
-			int res_size = 1;
+            res[0] = 1;
+            int res_size = 1;
 
-			for (int i = 2; i <= input; i++)
-			{
-				res_size = Multiply(i, res, res_size);
-			}
+            for (int i = 2; i <= input; i++)
+            {
+                res_size = Multiply(i, res, res_size);
+            }
 
-			for (int i = res_size - 1; i >= 0; i--)
-			{
-				Console.Write(res[i]);
-			}
-			Console.Read();
-		}
+            for (int i = res_size - 1; i >= 0; i--)
+            {
+                Console.Write(res[i]);
+            }
+            Console.Read();
+        }
 
 
         /// <summary>
@@ -79,28 +194,28 @@ namespace HowToCode
         /// <param name="res_size">Size of the array</param>
         /// <returns></returns>
 		private static int Multiply(int i, int[] res, int res_size)
-		{
+        {
             //Carry in case 
-			int carry = 0;
+            int carry = 0;
 
-			for (int j = 0; j < res_size; j++)
-			{
+            for (int j = 0; j < res_size; j++)
+            {
                 //Calculate the product by multiplying the numbers and adding the carry
-				int prod = res[j] * i + carry;
-				res[j] = prod % 10;
+                int prod = res[j] * i + carry;
+                res[j] = prod % 10;
 
-				carry = prod / 10;
-			}
+                carry = prod / 10;
+            }
 
             //Increment the array size and add the value of carry in the same
-			while (carry!=0)
-			{
-				res[res_size] = carry % 10;
-				carry = carry / 10;
-				res_size++;
-			}
+            while (carry != 0)
+            {
+                res[res_size] = carry % 10;
+                carry = carry / 10;
+                res_size++;
+            }
 
-			return res_size;
-		}
-	}
+            return res_size;
+        }
+    }
 }
